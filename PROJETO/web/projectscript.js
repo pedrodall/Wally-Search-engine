e2f767537703e061ca1b92a6d5934a04361b5771
Eduardo <53785691+Eduardovdlfg@users.noreply.github.com>
2020-06-08 21:20:59 -0300

    Organizando o repositório

---
+++ PROJETO/web/projectscript.js
@@ -0,0 +1,106 @@
+function query(){
+	window.value = 0;
+	//Obtenho o texto digitado pelo usuário
+	const text_query = document.getElementById("pesquisa").value;
+	//URL que farei a query para o servidor
+	const URL = "http://localhost:8080/query?text="+text_query;
+
+	//Acesso o URL "escondido", sem atualizar a página atual
+	fetch(URL)
+		.then(function (response) {  //e "então", executo essa função com a resposta do URL
+				return response.json();
+			  })
+		//chamamos esse "response.json()" de "data" e alteramos o q está escrito na tag de ig "output"
+		.then(function (data) {
+				document.querySelector("link[href='cssmainpage.css']").href = "cssquery.css";
+				document.getElementById("wally").innerHTML ="";
+				document.getElementById("atual").innerHTML = text_query;
+				document.getElementById("textoprépag").innerHTML="Número de páginas encontradas por Wally:";
+				document.getElementById("páginas").innerHTML = data.pag;
+				document.getElementById("páginaatual").innerHTML = "Página atual:"+(window.value+1)+"/"+data.pag;
+				for (var i = 1; i < 21 ; i++){
+					if(data.pesquisas[i-1] != null){
+						document.getElementById("div"+i).innerHTML=data.pesquisas[i-1];
+					}
+					else{document.getElementById("div"+i).innerHTML=""};	
+				}
+
+				document.getElementById("aba").innerHTML = "<form onsubmit=\'return false\' method=\'get\'> <a id=\'minilogo\' href=\'mainpage.html\'> <img src=\'waldinho.png\' width=10% height=10%></a><label for=\'pesquisa2\'> Mais alguma coisa? </label><input type=\'text\' id=\'pesquisa2\' autofocus required><p><input id=\'btn\' type=\'submit\' onclick=\'query2()\' value=\'Ok\'></form>";
+			  	
+			  })
+}
+
+function query2(){
+	window.value = 0;
+	//Obtenho o texto digitado pelo usuário
+	const text_query = document.getElementById("pesquisa2").value;
+	//URL que farei a query para o servidor
+	const URL = "http://localhost:8080/query?text="+text_query;
+
+	//Acesso o URL "escondido", sem atualizar a página atual
+	fetch(URL)
+		.then(function (response) {  //e "então", executo essa função com a resposta do URL
+				return response.json();
+			  })
+		//chamamos esse "response.json()" de "data" e alteramos o q está escrito na tag de ig "output"
+		.then(function (data) {
+
+				document.getElementById("wally").innerHTML ="";
+				document.getElementById("atual").innerHTML =text_query;
+				document.getElementById("textoprépag").innerHTML="Número de páginas encontradas por Wally:";
+				document.getElementById("páginas").innerHTML = data.pag;
+				document.getElementById("páginaatual").innerHTML = "Página atual:"+(window.value+1)+"/"+data.pag;
+				document.getElementById("div0").innerHTML = data.res;
+				for (var i = 1; i < 21 ; i++){
+					if(data.pesquisas[i-1] != null){
+						document.getElementById("div"+i).innerHTML=data.pesquisas[i-1];
+					}	
+					else{document.getElementById("div"+i).innerHTML="";};
+				}
+						  })
+}
+
+function expose(n,text_query){
+	const URL = "http://localhost:8080/query?text="+text_query;
+
+	//Acesso o URL "escondido", sem atualizar a página atual
+	fetch(URL)
+		.then(function (response) {  //e "então", executo essa função com a resposta do URL
+				return response.json();
+			  })
+		//chamamos esse "response.json()" de "data" e alteramos o q está escrito na tag de ig "output"
+		.then(function (data) {
+			document.getElementById("páginaatual").innerHTML = "Página atual:"+(n+1)+"/"+data.pag;
+				for (var i = 1; i < 21 ; i++){
+					if(data.pesquisas[20*n+i-1] != null){
+						document.getElementById("div"+i).innerHTML=data.pesquisas[20*n+i-1];
+					}
+					else{document.getElementById("div"+i).innerHTML="";};	
+				}
+
+			  })
+}
+
+function maisvt(){
+	if(window.value < document.getElementById("páginas").innerHTML -1){
+		window.value=window.value+1;
+		const text_query = document.getElementById("atual").innerHTML;
+
+		expose(window.value,text_query);
+	}
+}
+
+function menosvt(){
+	if(window.value>0){
+		window.value=window.value-1;
+		const text_query = document.getElementById("atual").innerHTML;
+		expose(window.value,text_query);
+	}
+}
+
+function print_img(){
+	const image_div = document.getElementById("image"); //Acesso a div que quero inserir a imagem
+	const image_obj = document.createElement("img"); //Crio uma tag vazia <img></img>
+	image_obj.src = "pokemon.jpeg"; //Altero o atributo "src", que é o endereço da imagem
+	image_div.appendChild(image_obj); //Insiro a tag criada dentro da <div id ="image"></div>
+}
